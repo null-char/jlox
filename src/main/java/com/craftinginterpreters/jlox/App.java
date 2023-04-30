cb19da565969187491715db17f4905491d4e54d5
Sooryakiran Ponnath <skp.frl@gmail.com>
2023-04-29 22:43:33 -0400

    initial commit

---
+++ src/main/java/com/craftinginterpreters/jlox/App.java
@@ -0,0 +1,12 @@
+package com.craftinginterpreters.jlox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Yay it works now!");
+  }
+}
